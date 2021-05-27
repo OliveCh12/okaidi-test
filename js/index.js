235f046af825bb544038a66bd52f9c7ba1443b65
OliveCh12 <olivierchemla@gmail.com>
2021-05-27 11:57:48 +0200

    Updating files

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 // Data fetched from opendb is stored inside an empty variable
 let questionsData;
 
+// State of current question
 let currentIndex = 0;
 let seconds = 30;
 
 // Generate random questions fetchin the opentdb api
-window.addEventListener("load", () => {
-  fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-    .then((response) => response.json())
-    .then((response) => (questionsData = response))
-    .catch((error) => alert("Erreur : " + error));
-});
+fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  .then((response) => response.json())
+  .then((response) => (questionsData = response))
+  .catch((error) => alert("Erreur : " + error));
 
 // Declare all variables representing the html tags present in the DOM
-const nextButton = document.querySelector("#next");
+const validateButton = document.querySelector("#validate");
 
+const questionBody = document.querySelector(".question__answers");
 const questionTitle = document.querySelector(".question__title");
 const questionCategory = document.querySelector(".question__category");
 const questionDifficulty = document.querySelector(".question__difficulty");
@@ -23,16 +23,14 @@ const questionResponse = document.querySelector(".question__response");
 const answersCheckbox = document.querySelectorAll(".question__checkbox");
 const answersLabel = document.querySelectorAll(".question__label");
 
-// Handling Next Question
-const handleNextQuestion = (currentQuestionIndex) => {
-  let question = questionsData.results[currentQuestionIndex];
-
-  console.log(currentQuestionIndex);
+// Render Question inside the DOM
+const renderQuestionData = (question) => {
+  questionBody.style = "display: block";
 
   // Display Data in DOM (Question Header)
   questionTitle.innerText = question.question;
   questionCategory.innerText = question.category;
-  questionIndex.innerText = `${currentIndex} / ${questionsData.results.length}`;
+  questionIndex.innerText = `${currentIndex + 1} / ${questionsData.results.length}`;
 
   // Display and define the color of the difficulty (Question Header)
   switch (question.difficulty) {
@@ -52,39 +50,46 @@ const handleNextQuestion = (currentQuestionIndex) => {
       questionDifficulty.innerText = "Difficulty";
       break;
   }
-  console.log("Correct :", question.correct_answer);
 
-  // Validate Answer for a given question
-  const seletedInput = document.querySelector('input[type="radio"]:checked');
-  const allInputs = document.querySelectorAll('input[type="radio"]');
+  // Randomize Answer in array (Question Body)
+  let allAnswers = [...question.incorrect_answers, question.correct_answer].sort(
+    () => Math.random() - 0.5
+  );
 
-  if (seletedInput === null) {
-    console.log("No Input");
-  } else if (seletedInput.value === question.correct_answer) {
-    questionResponse.innerText = "Last answer is correct ✅";
-  } else {
-    console.log("Yours :", seletedInput.value);
-    questionResponse.innerText = "Last answer is incorrect ❌";
-  }
-
-  // Set all inputs to false and remove active class
-  // allInputs.forEach((input) => {
-  //   input.checked = false;
-  //   input.parentNode.classList.remove("question__answer--active");
-  // });
-
-  // Randomize Answer (Question Body)
-  let allAnswers = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
-
+  // Display Data in each answer tag
   allAnswers.forEach((answer, index) => {
     answersLabel[index].innerText = answer;
     answersCheckbox[index].value = answer;
   });
-
-  console.warn(currentIndex, questionsData.results);
 };
 
-// Toggle Active Class for a selected Question
+// Validate Answer for a given question
+const validateAnswer = (question) => {
+  const seletedInput = document.querySelector('input[type="radio"]:checked');
+  const allInputs = document.querySelectorAll('input[type="radio"]');
+
+  // Check if selected input match to question.correct_answer
+  if (seletedInput === null) {
+    questionResponse.innerText = "No selected answer";
+  } else if (seletedInput.value === question.correct_answer) {
+    questionResponse.innerText = "Last answer is correct ✅";
+    seconds = 30;
+    currentIndex++;
+  } else {
+    console.table("Yours :", seletedInput.value, "Correct:", question.correct_answer);
+    questionResponse.innerText = "Last answer is incorrect ❌";
+    seconds = 30;
+    currentIndex++;
+  }
+
+  // Set all inputs to false and remove active class
+  allInputs.forEach((input) => {
+    input.checked = false;
+    input.parentNode.classList.remove("question__answer--active");
+  });
+};
+
+// Toggle active class for a selected Question with Event Listener
 const questionAnswers = document.querySelectorAll(".question__answer");
 const clickAudio = new Audio("../assets/sounds/click.ogg");
 
@@ -93,41 +98,37 @@ questionAnswers.forEach((item) => {
     clickAudio.play();
     if (item.childNodes[1].checked) {
       item.childNodes[1].checked = false;
-      // console.log(document.querySelector('input[type="radio"]:checked').value);
-      // item.classList.remove("question__answer--active");
+      item.classList.remove("question__answer--active");
     } else {
       item.childNodes[1].checked = true;
-      // console.log(document.querySelector('input[type="radio"]:checked').value);
-
-      // item.classList.add("question__answer--active");
+      item.classList.add("question__answer--active");
     }
   });
 });
 
 // Timer Logic
-//const questionTimer = document.querySelector(".question__timer");
+const questionTimer = document.querySelector(".question__timer");
 
-// const timer = setInterval(() => {
-//   questionTimer.innerHTML = `${seconds--} s`;
-//   if (seconds === 0) {
-//     seconds = 30;
-//     handleNextQuestion();
-//   }
-// }, 1000);
+const timer = setInterval(() => {
+  questionTimer.innerHTML = `${seconds--} s`;
+  if (seconds === 0) {
+    seconds = 30;
+    validateButton.click();
+  }
+}, 1000);
 
-// Validate Answer for question
-
-// const startButton = document.querySelector("#start");
-// const nextButton = document.querySelector("#next");
-
-nextButton.addEventListener("click", () => {
-  if (currentIndex === 0) {
-    nextButton.innerText = "Next";
-    handleNextQuestion(0);
-    currentIndex++;
-  } else if (currentIndex === 5) {
-    alert("Finish");
+validateButton.addEventListener("click", () => {
+  if (currentIndex >= questionsData.results.length) {
+    alert("The Quizz is finish, please reload");
   } else {
-    handleNextQuestion(currentIndex++);
+    validateButton.innerText = "Validate";
+    validateAnswer(questionsData.results[currentIndex]);
+    renderQuestionData(questionsData.results[currentIndex]);
+
+    // Console.Log Correct Answer
+    console.log({
+      id: currentIndex,
+      correct: questionsData.results[currentIndex].correct_answer,
+    });
   }
 });
